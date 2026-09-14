99b01d5937048b58ae0f40921f596e83a8197a16
Sophia Bahaa <sofiabahaa20@gmail.com>
2026-09-15 02:46:18 +0300

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -7,14 +7,18 @@ terraform {
       version = "~> 5.0"
     }
   }
-
-  # backend "s3" {
-  #   bucket = "your-terraform-state-bucket"
-  #   key    = "vpc/terraform.tfstate"
-  #   region = "us-east-1"
-  # }
 }
 
 provider "aws" {
-  region = var.aws_region
+  region                      = "us-east-1"
+  access_key                  = "test"
+  secret_key                  = "test"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  s3_use_path_style           = true
+
+  endpoints {
+    ec2 = "http://floci:4566"
+  }
 }
